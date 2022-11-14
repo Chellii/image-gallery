b2667a7572e21e0a82d951bb0b332544dcb66fea
chellii <eloiriaglichaimae2@gmail.com>
2022-11-14 10:26:57 +0100

    adding static data

---
+++ __data__/userData.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        username: 'muser1',
+        password: 'mpassword1',
+        status: 'Active'
+    },
+    {
+        username: 'muser2',
+        password: 'mpassword2',
+        status: 'Active'
+    },
+    {
+        username: 'muser3',
+        password: 'mpassword3',
+        status: 'Blocked'
+    },
+]
