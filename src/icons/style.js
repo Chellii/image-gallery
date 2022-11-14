2b0a2466d6670d96ecedf53f4ba15b75a0f40386
chellii <eloiriaglichaimae2@gmail.com>
2022-11-14 10:28:22 +0100

    adding icons

---
+++ src/icons/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const SvgFilled = styled.svg` 
+    fill: red;
+    height: 60px; 
+    transition: all 500ms ease-in-out;
+    width: 50px;
+    height: 30px;
+`
+
+export const SvgStroke = styled.svg` 
+    height: 60px; 
+    fill: white; 
+    transition: all 500ms ease-in-out;
+    width: 50px;
+    height: 30px;
+`
